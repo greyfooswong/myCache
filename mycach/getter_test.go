5322dddc4bf8f918b6083bc93a3f5ce7c32c9259
southli <961065551@qq.com>
2021-11-04 14:27:44 +0800

    测试getter和group的get方法是否可以正确使用

---
+++ mycach/getter_test.go
@@ -0,0 +1,50 @@
+package myCach
+
+import (
+	"fmt"
+	"log"
+	"reflect"
+	"testing"
+)
+
+func TestGetter(t *testing.T) {
+	var f Getter = GetterFunc(func(key string) ([]byte, error) {
+		return []byte(key), nil
+	})
+	expect := []byte("key")
+	if v, _ := f.Get("key"); !reflect.DeepEqual(v, expect) {
+		t.Errorf("callback failed")
+	}
+}
+
+var db = map[string]string{
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
+}
+
+func TestGet(t *testing.T) {
+	loadCounts := make(map[string]int, len(db))
+	cac := NewGroup("scores", 2<<10, GetterFunc(func(key string) ([]byte, error) {
+		log.Println("[DB] search Key", key)
+		if v, ok := db[key]; ok {
+			if _, ok := loadCounts[key]; !ok {
+				loadCounts[key] = 0
+			}
+			loadCounts[key] += 1
+			return []byte(v), nil
+		}
+		return nil, fmt.Errorf("%s not exist", key)
+	}))
+	for k, v := range db {
+		if view, err := cac.Get(k); err != nil || view.String() != v {
+			t.Fatal("failed to get value of Tom")
+		}
+		if _, err := cac.Get(k); err != nil || loadCounts[k] > 1 {
+			t.Fatalf("cache %s miss", k)
+		}
+	}
+	if view, err := cac.Get("unknown"); err == nil {
+		t.Fatalf("the value of unknow shold be empty, but %s got", view)
+	}
+}
