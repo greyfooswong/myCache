ae4f0949d033b413ad4f26fedabfa821d4ab3fc5
southli <961065551@qq.com>
2021-11-04 17:19:58 +0800

    startCacheServer启动缓存服务器 startAPIServer启动一个API服务，与用户进行交互，用户感知

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"myCache/mycach"
@@ -14,7 +15,30 @@ var db = map[string]string{
 }
 
 func main() {
-	myCach.NewGroup("scores", 2<<10, myCach.GetterFunc(
+	var port int
+	var api bool
+	flag.IntVar(&port, "port", 8001, "Mycache server port")
+	flag.BoolVar(&api, "api", false, "Start a papi server?")
+	flag.Parse()
+	apiAddr := "http://localhost:9999"
+	addrMap := map[int]string{
+		8001: "http://localhost:8001",
+		8002: "http://localhost:8002",
+		8003: "http://localhost:8003",
+	}
+	var addrs []string
+	for _, v := range addrMap {
+		addrs = append(addrs, v)
+	}
+	group := createGroup()
+	if api {
+		go startAPIServer(apiAddr, group)
+	}
+	startCacheServer(addrMap[port], []string(addrs), group)
+}
+
+func createGroup() *myCach.Group {
+	return myCach.NewGroup("scores", 2<<10, myCach.GetterFunc(
 		func(key string) ([]byte, error) {
 			log.Println("[SlowDB] search key", key)
 			if v, ok := db[key]; ok {
@@ -23,8 +47,27 @@ func main() {
 			return nil, fmt.Errorf("%s not exist", key)
 		},
 	))
-	addr := "localhost:9999"
+}
+
+func startAPIServer(apiAddr string, group *myCach.Group) {
+	http.Handle("/api", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		key := request.URL.Query().Get("key")
+		view, err := group.Get(key)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		writer.Header().Set("Content-Type", "application/octet-stream")
+		writer.Write(view.ByteSlice())
+	}))
+	log.Println("fontend server is running at", apiAddr)
+	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
+}
+
+func startCacheServer(addr string, addrs []string, group *myCach.Group) {
 	peers := myCach.NewHTTPPool(addr)
+	peers.Set(addrs...)
+	group.RegisterPeers(peers)
 	log.Println("myCache is running at", addr)
-	log.Fatal(http.ListenAndServe(addr, peers))
+	log.Fatal(http.ListenAndServe(addr[7:], peers))
 }
