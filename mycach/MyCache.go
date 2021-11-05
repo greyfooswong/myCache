7c160a90cc9a8df4dc1eaee65e5f6b20bbf816ce
southli <961065551@qq.com>
2021-11-05 10:23:57 +0800

    调整依赖pb的接口与方法等

--- mycach/MyCache.go
+++ mycach/MyCache.go
@@ -3,6 +3,7 @@ package myCach
 import (
 	"fmt"
 	"log"
+	pb "myCache/mycach/myCachepb"
 	"myCache/mycach/singleflight"
 	"sync"
 )
@@ -51,11 +52,16 @@ func (g *Group) load(key string) (value ByteView, err error) {
 }
 
 func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
-	bytes, err := peer.Get(g.name, key)
+	req := &pb.Request{
+		Group: g.name,
+		Key:   key,
+	}
+	res := &pb.Response{}
+	err := peer.Get(req, res)
 	if err != nil {
 		return ByteView{}, err
 	}
-	return ByteView{b: bytes}, nil
+	return ByteView{b: res.Value}, nil
 }
 
 var (
--- mycach/http.go
+++ mycach/http.go
@@ -2,9 +2,11 @@ package myCach
 
 import (
 	"fmt"
+	"google.golang.org/protobuf/proto"
 	"io/ioutil"
 	"log"
 	"myCache/mycach/consistenthash"
+	pb "myCache/mycach/myCachepb"
 	"net/http"
 	"net/url"
 	"strings"
@@ -58,31 +60,37 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	body, err := proto.Marshal(&pb.Response{Value: view.ByteSlice()})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	w.Header().Set("Content-Type", "application/octet-stream")
-	w.Write(view.ByteSlice())
+	w.Write(body)
 }
 
 type httpGetter struct {
 	baseURL string
 }
 
-func (h *httpGetter) Get(group, key string) ([]byte, error) {
-	u := fmt.Sprintf("%v%v/%v", h.baseURL, url.QueryEscape(group), url.QueryEscape(key))
+func (h *httpGetter) Get(in *pb.Request, out *pb.Response) error {
+	u := fmt.Sprintf("%v%v/%v", h.baseURL, url.QueryEscape(in.GetGroup()), url.QueryEscape(in.GetKey()))
 	res, err := http.Get(u)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("server returned: %v", res.Status)
+		return fmt.Errorf("server returned: %v", res.Status)
 	}
 
 	bytes, err := ioutil.ReadAll(res.Body)
+	err = proto.Unmarshal(bytes, out)
 	if err != nil {
-		return nil, fmt.Errorf("reading response body : %v", err)
+		return fmt.Errorf("reading response body : %v", err)
 	}
-	return bytes, nil
+	return nil
 }
 
 var _ PeerGetter = (*httpGetter)(nil)
